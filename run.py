95736a1b99e8f4e807d7b7b0c09e7a87ed79c8ee
GeorgePh0 <george.philip0020@gmail.com>
2023-11-03 10:56:51 +0000

    Add if statement for user to see the rules

--- run.py
+++ run.py
@@ -2,9 +2,18 @@
 import random
 
 def welcome():
+    """
+    Function prints a welcome message and asks the user
+    if they want to learn the rules or go into the game
+    """
     print("Welcome to Rock Paper Scissor. \n"
     "If you want to learn the rules of the game then press 'y' \n"
     "If you want to go straight into the game then press 'n' \n")
+    rules = input("")
+    if rules == 'y':
+        print("Here are the rules")
+    else:
+        print("Go into the game")
 
 
 welcome()
