b9d37aecdee98839aad80597d563852b249fd4ca
GeorgePh0 <george.philip0020@gmail.com>
2023-11-03 11:52:22 +0000

    Add player name with a while loop and if statement

--- run.py
+++ run.py
@@ -6,15 +6,16 @@ def welcome():
     Function prints a welcome message and asks the user
     if they want to learn the rules or go into the game
     """
-    print("Welcome to Rock Paper Scissor. \n"
-    "If you want to learn the rules of the game then press 'y' \n"
-    "If you want to go straight into the game then press 'n' \n")
-    rules = input("")
-    if rules == 'y':
-        print("Here are the rules")
-    else:
-        print("Go into the game")
-
+    print("Welcome to Rock Paper Scissor. \n")
+    player = " "
+    while True:
+        player = input("Please enter your name: ")
+        if player.isalpha() is not True:
+            print("Error: Please enter letters only")
+            continue
+        else:
+            print(f'Hi {player}, welcome to the game')
+            return player
 
 welcome()
 
